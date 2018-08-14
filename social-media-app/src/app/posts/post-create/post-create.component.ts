f11fae83cae8d45b8254c1d1d5bd80af7829c967
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2018-08-14 17:37:46 -0500

    Created Post Model

--- social-media-app/src/app/posts/post-create/post-create.component.ts
+++ social-media-app/src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
+import { Post } from '../post.model';
+
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -9,7 +11,7 @@ export class PostCreateComponent implements OnInit {
 
   enteredContent = '';
   enteredTitle = '';
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
 
   constructor() { }
@@ -19,7 +21,7 @@ export class PostCreateComponent implements OnInit {
   }
 
   onAddPost() {
-    const post = {
+    const post: Post = {
       title: this.enteredTitle,
       content: this.enteredContent
     };
--- social-media-app/src/app/posts/post-list/post-list.component.ts
+++ social-media-app/src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { Post } from '../post.model';
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -13,7 +15,7 @@ export class PostListComponent implements OnInit {
   //   {title: 'Third Post', content: 'This is a post'}
   // ];
 
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
 
   constructor() { }
 
