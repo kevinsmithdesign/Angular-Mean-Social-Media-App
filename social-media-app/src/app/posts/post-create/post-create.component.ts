e982d87a90c5c77c7fd143695e209ca704219eec
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2018-08-14 17:55:37 -0500

    Added form validation

--- social-media-app/src/app/posts/post-create/post-create.component.ts
+++ social-media-app/src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -20,10 +21,13 @@ export class PostCreateComponent implements OnInit {
 
   }
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
